e350759c034d741cd0776c0bd2bc3c03baf4dd2b
Christopher Zeisel <38956932+czeisel@users.noreply.github.com>
2020-11-23 00:51:21 -0600

    Create CalculatePnL.cs

---
+++ MarkitWSO/Metrics/CalculatePnL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkitWSO.Metrics
+{
+    //(Convert.ToDouble(tbSharesSold.Text) * Convert.ToDouble(tbPricePerShare.Text) - soldShareCost).ToString();//PnL
+    class CalculatePnL
+    {
+        public double PnL(int BuyShareCount, double BuyPricePerShare, double SellPerShare)
+        {
+            return ((BuyShareCount * BuyPricePerShare) - SellPerShare);
+        }
+    }
+}
