aa9540ee5fcca648cd2cde6a6ee6e45ffbee1222
Christopher Zeisel <38956932+czeisel@users.noreply.github.com>
2020-11-23 00:51:29 -0600

    Create TradeCalculator.cs

---
+++ MarkitWSO/Metrics/TradeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkitWSO.Metrics
+{
+    class TradeCalculator
+    {
+        /// <summary>
+        /// Shares Cost - Artifact Candidate
+        /// </summary>
+        /// <returns></returns>
+        public double Cost(int count, double price)
+        {
+            return (count * price);
+        }
+
+        /// <summary>
+        /// Profit and Loss - Artifact Candidate
+        /// </summary>
+        /// <returns></returns>
+        public double PnL()
+        {
+            return 0.0;
+        }
+
+
+            
+    }
+}
