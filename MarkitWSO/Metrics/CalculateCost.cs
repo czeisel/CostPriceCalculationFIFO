67460cfca222281c82e3c6594dee4d3efc6edb4b
Christopher Zeisel <38956932+czeisel@users.noreply.github.com>
2020-11-23 00:51:16 -0600

    Create CalculateCost.cs

---
+++ MarkitWSO/Metrics/CalculateCost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkitWSO.Metrics
+{
+    class CalculateCost
+    {
+        /// <summary>
+        /// Shares Cost - Artifact Candidate
+        /// </summary>
+        /// <returns></returns>
+        public double Cost(int count, double price)
+        {
+            return (count * price);
+        }
+    }
+}
